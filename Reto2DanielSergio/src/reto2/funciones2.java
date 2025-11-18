bae628a1f762c28424a0ee872881798c44c9b279
DanielCarro20 <danie@Daniel>
2025-11-18 12:01:44 +0100

    qwdqwd

--- Reto2DanielSergio/src/reto2/funciones2.java
+++ Reto2DanielSergio/src/reto2/funciones2.java
@@ -1,5 +1,37 @@
 package reto2;
 
 public class funciones2 {
-
+	public static double celsiusAFahrenheit(double c) {
+		return c * 9 / 5 + 32;
+	}
+	
+	
+	public static double fahrenheitACelsius(double f) {
+			return (f-32) * 5/9;
+	}
+	
+	
+	public static double celsiusAKelvin(double celsius) {
+		return celsius + 273.15;
+		
+	}
+	
+	public static double kelvinACelsius (double kelvin) {
+		return kelvin - 273.15;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
