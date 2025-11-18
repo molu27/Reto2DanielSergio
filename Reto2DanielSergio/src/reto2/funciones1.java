846e77ffe295220fce2783805d4d8047ade9ae26
molu27 <1DAM@A11-PC25>
2025-11-18 12:26:50 +0100

    hola

--- Reto2DanielSergio/src/reto2/funciones1.java
+++ Reto2DanielSergio/src/reto2/funciones1.java
@@ -1,5 +1,7 @@
 package reto2;
 
+import java.util.Scanner;
+
 public class funciones1 {
 
 	public static double MillasAKm(double km, double millas) {
@@ -16,6 +18,37 @@ public class funciones1 {
 
 	public static double LibrasAKg(double kg, double libras) {
 		return 0.453592 * libras;
+	}
+	public static void MenuLongitud(Scanner sc) {
+		int num = 0;
+		do {
+		System.out.println("Selecciona la conversión de longitud: ");
+		System.out.println("1. Millas a Kilómetros: ");
+		System.out.println("2. Kilómetros a Millas: ");
+		System.out.println("0. Salir: ");
+		String numt = sc.nextLine();
+		num = Integer.parseInt(numt);
+		int n1 = 0;
+		int n2 = 0;
+		
+		switch (num) {
+		case 1: {
+			
+			break;
+		}
+		case 2: {
+			break;
+		}
+		case 0: {
+			System.out.println("Salir");
+			break;
+		}
+		default:
+			System.out.println("Te has equivocado");
+		}
+		
+		
+		} while (num != 0);
 	}
 
 }
--- Reto2DanielSergio/src/reto2/reto2.java
+++ Reto2DanielSergio/src/reto2/reto2.java
@@ -1,10 +1,15 @@
 package reto2;
 
+import java.util.Scanner;
+
+import reto2.funciones1;
+import reto2.funciones2;
+import reto2.Funciones;
+
 public class reto2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
