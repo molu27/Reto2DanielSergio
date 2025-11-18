8e1757ef1cf274dfad0e854e7330f8a385fea218
molu27 <1DAM@A11-PC25>
2025-11-18 13:03:52 +0100

    final

--- Reto2DanielSergio/src/reto2/funciones1.java
+++ Reto2DanielSergio/src/reto2/funciones1.java
@@ -4,51 +4,84 @@ import java.util.Scanner;
 
 public class funciones1 {
 
-	public static double MillasAKm(double km, double millas) {
+	public static double MillasAKm(double millas) {
 		return millas * 1.60934;
 	}
 
-	public static double KmAMillas(double km, double millas) {
-		return km / millas;
+	public static double KmAMillas(double km) {
+		return km * 0.621371;
 	}
 
-	public static double KgALibras(double kg, double libras) {
+	public static double KgALibras(double kg) {
 		return kg * 2.20462;
 	}
 
-	public static double LibrasAKg(double kg, double libras) {
+	public static double LibrasAKg(double libras) {
 		return 0.453592 * libras;
 	}
+
 	public static void MenuLongitud(Scanner sc) {
-		int num = 0;
+		int opcion = 0;
+		double n1 = 0;
 		do {
-		System.out.println("Selecciona la conversión de longitud: ");
-		System.out.println("1. Millas a Kilómetros: ");
-		System.out.println("2. Kilómetros a Millas: ");
-		System.out.println("0. Salir: ");
-		String numt = sc.nextLine();
-		num = Integer.parseInt(numt);
-		int n1 = 0;
-		int n2 = 0;
-		
-		switch (num) {
-		case 1: {
+			System.out.println("Selecciona la conversión de longitud: ");
+			System.out.println("1. Millas a Kilómetros: ");
+			System.out.println("2. Kilómetros a Millas: ");
+			System.out.println("0. Salir: ");
+			String numt = sc.nextLine();
+			opcion = Integer.parseInt(numt);
 			
-			break;
+
+			switch (opcion) {
+			case 1:
+				n1 = Funciones.dimeDouble("Dime numero", sc);
+				System.out.println("Millas a Km: " + (funciones1.MillasAKm(n1)));
+				break;
+			case 2:
+				n1 = Funciones.dimeDouble("Dime numero", sc);
+				System.out.println("Km a Millas: " + (funciones1.KmAMillas(n1)));
+				break;
+			case 0:
+				System.out.println("Salir");
+				break;
+			default:
+				System.out.println("Te has equivocado");
+				break;
+			}
+		} while (opcion != 0);
+	}
+	public static void MenuPeso(Scanner sc) {
+		int opcion = 0;
+		double n1 = 0;
+		do {
+			System.out.println("Selecciona la conversión de longitud: ");
+			System.out.println("1. Kg a Libras: ");
+			System.out.println("2. Libras a Kg: ");
+			System.out.println("0. Salir: ");
+			String numt = sc.nextLine();
+			opcion = Integer.parseInt(numt);
+			
+			switch (opcion) {
+			case 1:
+				n1 = Funciones.dimeDouble("Dime numero", sc);
+				System.out.println("Kg a Libras: " + (funciones1.KgALibras(n1)));
+				break;
+			case 2:
+				n1 = Funciones.dimeDouble("Dime numero", sc);
+				System.out.println("Libras a Kg: " + (funciones1.LibrasAKg(n1)));
+				break;
+			case 0:
+				System.out.println("Salir");
+				break;
+			default:
+				System.out.println("Te has equivocado");
+				break;
+			}
+		} while (opcion != 0);
 		}
-		case 2: {
-			break;
-		}
-		case 0: {
-			System.out.println("Salir");
-			break;
-		}
-		default:
-			System.out.println("Te has equivocado");
-		}
+			
 		
-		
-		} while (num != 0);
 	}
 
-}
+
+
--- Reto2DanielSergio/src/reto2/reto2.java
+++ Reto2DanielSergio/src/reto2/reto2.java
@@ -9,7 +9,50 @@ import reto2.Funciones;
 public class reto2 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int opcion = 0;
+		double n1 = 0;
+	do {
+		System.out.println("1. Longitud");
+		System.out.println("2. Peso");
+		System.out.println("3. Temperatura");
+		System.out.println("4. Volumen");
+		System.out.println("0. Salir");
+		System.out.println("Elige la operación:");
+		String numero = sc.nextLine();
+		opcion = Integer.parseInt(numero);
 		
+		
+		switch (opcion) {
+		
+		case 1:
+			//Aquí invocamos a la función de MenuLongitud que habíamos configurado en funciones 1
+			funciones1.MenuLongitud(sc);
+			break;
+			
+		case 2:
+			//Aquí invocamos a la función de MenuLongitud que habíamos configurado en funciones 1
+			funciones1.MenuPeso(sc);
+			break;
+		case 3:
+			//Aquí invocamos a la función de MenuLongitud que habíamos configurado en funciones 2
+			funciones2.MenuTemperatura(sc);
+			break;
+		case 4:
+			//Aquí invocamos a la función de MenuLongitud que habíamos configurado en funciones 2
+			funciones2.MenuVolumen(sc);
+			break;
+		case 0:
+			System.out.println("Salir");
+			break;
+			
+		default:
+		System.out.println("Te has equivocado");
+			break;
+		}
+	} while (opcion !=0);
 	}
 
-}
+	}
+
+
