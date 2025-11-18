58782cd704f522687e35b42ec3854a120dae814a
DanielCarro20 <danie@Daniel>
2025-11-18 12:19:16 +0100

    wdqd

--- Reto2DanielSergio/src/reto2/funciones2.java
+++ Reto2DanielSergio/src/reto2/funciones2.java
@@ -1,5 +1,7 @@
 package reto2;
 
+import java.util.Scanner;
+
 public class funciones2 {
 	public static double celsiusAFahrenheit(double c) {
 		return c * 9 / 5 + 32;
@@ -11,16 +13,32 @@ public class funciones2 {
 	}
 	
 	
-	public static double celsiusAKelvin(double celsius) {
-		return celsius + 273.15;
+
+
+    public static double litrosAGalones(double litros) {
+        return litros * 0.264172;
+    }
+
+   
+    public static double galonesALitros(double galones) {
+        return galones * 3.78541;
+    }
+
+	
+	public static void MenuTemperatura (Scanner sc) {
+		System.out.println("Selecciona la temperatura conversión");
+		System.out.println("Celsius a Fahrenheit");
+		System.out.println("Fahrenheit a Celsius");
+	
 		
 	}
 	
-	public static double kelvinACelsius (double kelvin) {
-		return kelvin - 273.15;
+	public static void MenuVolumen (Scanner sc) {
+		System.out.println("Selecciona el volumen conversión");
+		System.out.println("Litros a Galones");
+		System.out.println("Galones a Litros");
+		
 	}
-	
-	
 	
 	
 	
