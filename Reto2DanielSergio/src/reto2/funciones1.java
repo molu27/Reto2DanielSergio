4a66fbe968fcb712f3daffe74a56dfbd69eede6e
molu27 <1DAM@A11-PC25>
2025-11-18 11:57:52 +0100

    actualizacion

--- Reto2DanielSergio/src/reto2/funciones1.java
+++ Reto2DanielSergio/src/reto2/funciones1.java
@@ -2,4 +2,20 @@ package reto2;
 
 public class funciones1 {
 
+	public static double MillasAKm(double km, double millas) {
+		return millas * 1.60934;
+	}
+
+	public static double KmAMillas(double km, double millas) {
+		return km / millas;
+	}
+
+	public static double KgALibras(double kg, double libras) {
+		return kg * 2.20462;
+	}
+
+	public static double LibrasAKg(double kg, double libras) {
+		return 0.453592 * libras;
+	}
+
 }
